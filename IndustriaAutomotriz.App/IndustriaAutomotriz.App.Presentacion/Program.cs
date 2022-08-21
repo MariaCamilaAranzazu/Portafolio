941924fb2098e855c8c46169d90463df04dd27f7
EdwardTel <edward_tel@yahoo.es>
2022-08-20 19:37:13 -0500

    Primer Commit

---
+++ IndustriaAutomotriz.App/IndustriaAutomotriz.App.Presentacion/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using IndustriaAutomotriz.App.Dominio;
+
+namespace IndustriaAutomotriz.app.Presentacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola a todos");
+        }
+    }
+}
