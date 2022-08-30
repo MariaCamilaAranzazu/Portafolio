6c6e55402c34b33f593be3eeb107e9f00b28412c
EdwardTel <edward_tel@yahoo.es>
2022-08-30 13:15:13 -0500

    Primera migracion con Entity Framework

---
+++ IndustriaAutomotriz/Persistencia/Migrations/20220830180647_Inicial.cs
@@ -0,0 +1,218 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistencia.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccesosClientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClienteCedula = table.Column<int>(type: "int", nullable: false),
+                    usuario = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    contraseña = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    fecha_creacion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccesosClientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccesosEmpleados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpleadoCedula = table.Column<int>(type: "int", nullable: false),
+                    contraseña = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    fecha_creacion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccesosEmpleados", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cargos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    fecha_creacion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cargos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Cedula = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    telefono = table.Column<int>(type: "int", nullable: false),
+                    direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    correo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    fecha_creacion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Cedula);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Empleados",
+                columns: table => new
+                {
+                    Cedula = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    cargo_id = table.Column<int>(type: "int", nullable: false),
+                    telefono = table.Column<int>(type: "int", nullable: false),
+                    direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    correo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    fecha_creacion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleados", x => x.Cedula);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrdenesServicios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    rev_id = table.Column<int>(type: "int", nullable: false),
+                    emp_cedula = table.Column<int>(type: "int", nullable: false),
+                    act_software = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    inst_repuestos = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    costo_respuestos = table.Column<double>(type: "float", nullable: false),
+                    costo_servicio = table.Column<double>(type: "float", nullable: false),
+                    total_pagar = table.Column<double>(type: "float", nullable: false),
+                    fecha_creacion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    observaciones = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrdenesServicios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Repuestos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    cantidad = table.Column<int>(type: "int", nullable: false),
+                    precio_unidad = table.Column<int>(type: "int", nullable: false),
+                    fecha_creacion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Repuestos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RepuestosxServicios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrdenServicioId = table.Column<int>(type: "int", nullable: false),
+                    RepuestoId = table.Column<int>(type: "int", nullable: false),
+                    cant_actual = table.Column<int>(type: "int", nullable: false),
+                    cant_sal_ent = table.Column<int>(type: "int", nullable: false),
+                    valor_pagar = table.Column<int>(type: "int", nullable: false),
+                    fecha_creacion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RepuestosxServicios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Revisiones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClienteCedula = table.Column<int>(type: "int", nullable: false),
+                    EmpleadoCedula = table.Column<int>(type: "int", nullable: false),
+                    VehiculoPlaca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    est_aceite = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    est_filtro_aire = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    est_filtro_gasolina = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    tipo_mantenimiento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    fecha_creacion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    observaciones = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Revisiones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehiculos",
+                columns: table => new
+                {
+                    Placa = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    tipo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    cli_cedula = table.Column<int>(type: "int", nullable: false),
+                    marca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    modelo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    color = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    fecha_creacion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehiculos", x => x.Placa);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccesosClientes");
+
+            migrationBuilder.DropTable(
+                name: "AccesosEmpleados");
+
+            migrationBuilder.DropTable(
+                name: "Cargos");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Empleados");
+
+            migrationBuilder.DropTable(
+                name: "OrdenesServicios");
+
+            migrationBuilder.DropTable(
+                name: "Repuestos");
+
+            migrationBuilder.DropTable(
+                name: "RepuestosxServicios");
+
+            migrationBuilder.DropTable(
+                name: "Revisiones");
+
+            migrationBuilder.DropTable(
+                name: "Vehiculos");
+        }
+    }
+}
