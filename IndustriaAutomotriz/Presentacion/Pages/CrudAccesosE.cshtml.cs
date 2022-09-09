b675917e649f6b8bc8f6753a18a99bb8229ba257
MariaCamilaAranzazu <camilaaranzazur@gmail.com>
2022-09-09 18:48:56 -0500

    Creacion del CRUD de acceso empleados

---
+++ IndustriaAutomotriz/Presentacion/Pages/CrudAccesosE.cshtml.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Dominio.Entidades;
+using Persistencia.AppRepositorios;
+
+namespace Presentacion.Pages
+{
+    public class CrudAccesosE : PageModel
+    {
+        /*private readonly ILogger<CrudAccesosE> _logger;
+
+        public CrudAccesosE(ILogger<CrudAccesosE> logger)
+        {
+            _logger = logger;
+        }*/
+
+        private readonly IRepository<AccesoEmpleado> repoAccesoE;
+        public IEnumerable<AccesoEmpleado> accesosE {get;set;}
+
+        public CrudAccesosE(IRepository<AccesoEmpleado> repoAccesoE)
+        {
+            this.repoAccesoE = repoAccesoE;
+        }
+
+        public void OnGet()
+        {
+            accesosE = repoAccesoE.GetAll().Result;
+        }
+
+        [BindProperty]
+        public AccesoEmpleado NuevoAccesoE {get;set;} 
+        [BindProperty]
+        public AccesoEmpleado AccesoEEditar {get;set;}
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid==false)
+            {
+                return Page();
+            }
+            repoAccesoE.Insert(NuevoAccesoE);
+            return RedirectToPage("/CrudAccesosE");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            var accesoE = repoAccesoE.GetById(id).Result;
+            if (accesoE == null)
+            {
+                return NotFound();
+            }
+
+            repoAccesoE.Delete(accesoE);
+            return RedirectToPage("/CrudAccesosE");
+        }
+
+        public IActionResult OnPostEdit()
+        {
+            Console.WriteLine("Se encontró: " + AccesoEEditar.EmpleadoCedula);
+            var accesoE = repoAccesoE.GetBy(ae => ae.EmpleadoCedula == AccesoEEditar.EmpleadoCedula).Result;
+            if (accesoE == null)
+            {
+                return NotFound();
+            }
+            accesoE.EmpleadoCedula = AccesoEEditar.EmpleadoCedula;
+            accesoE.Contraseña = AccesoEEditar.Contraseña;
+            repoAccesoE.Update(accesoE);
+
+            return RedirectToPage("/CrudAccesosE");
+        }
+    }
+}
