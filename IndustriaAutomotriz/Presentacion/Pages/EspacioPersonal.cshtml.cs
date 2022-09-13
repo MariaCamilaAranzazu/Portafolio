30ede756dc8b9da9fa0a5a1f299ee9e0fa8852ca
MariaCamilaAranzazu <camilaaranzazur@gmail.com>
2022-09-13 10:06:17 -0500

    se agrega vista de cliente - parcialmente terminada

---
+++ IndustriaAutomotriz/Presentacion/Pages/EspacioPersonal.cshtml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Presentacion.Pages
+{
+    public class EspacioPersonal : PageModel
+    {
+        /*private readonly ILogger<EspacioPersonal> _logger;
+
+        public EspacioPersonal(ILogger<EspacioPersonal> logger)
+        {
+            _logger = logger;
+        }*/
+
+        public void OnGet()
+        {
+        }
+    }
+}
